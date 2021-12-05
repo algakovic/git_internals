ee8144d978bb0d0e5b022682141468a402914e8a
Aleksandar Gakovic <algakovic@gmail.com>
2021-12-05 16:13:11 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup (name = 'ugit',
+        version = '1.0',
+        pacalges = ['ugit'].
+        entry_points = {
+            'console_scripts' : [
+                'ugit = ugit.cli:main'
+                ]
+            }]
